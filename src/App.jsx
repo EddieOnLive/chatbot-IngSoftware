825fe0f50a6ca22d76f4ffc31b7f16c0274fc930
EddieOnLive <eduardowelchenhein@gmail.com>
2024-04-02 09:19:52 -0400

    Chatbot visualmente incorporado + tailwind funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import ChatBot from "react-simple-chatbot";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div><h1>Hola Mundo</h1></div>
+      <div className="bg-slate-800 flex justify-center">
+        <div>
+          <ChatBot
+            steps={[
+              {
+                id: "hello-world",
+                message: "Hello World!",
+                end: true,
+              },
+            ]}
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="bg-slate-800">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    ,
+  </div>
+);
